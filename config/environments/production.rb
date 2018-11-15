f92428c88a7a05af984b69e354b5ecda0090ca20
Kishan Patel <kishanptl.kp@gmail.com>
2018-11-15 23:53:20 +0530

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,8 +99,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            'kishankpatel432@gmail.com',
-    password:             'test@kishan',
+    user_name:            'test@gmail.com',
+    password:             'test@1234',
     authentication:       :login,
     enable_starttls_auto: true
   }
